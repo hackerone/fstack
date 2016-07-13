957a3a668082a5a7c91829338b918b6f7f625d14
Shanmugasundaram <ganeshks@amazon.com>
2016-07-13 08:31:39 +0100

    prune package.json & add batch build

--- server/dev.js
+++ server/dev.js
@@ -1,21 +1,20 @@
 import fs from 'fs';
-import path from 'path';
 import webpackDevMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
 import webpack from 'webpack';
 import app from './app';
 import config from './../webpack.dev';
 
-const indexView = fs.readFileSync( __dirname + '/views/index.html', 'utf8')
-  .replace('[main.js]', cdn + 'app.js')
-  .replace('[main.css]', cdn + 'main.css');
+const compiler = webpack(config);
+const cdn = '/client/';
+const indexView = fs.readFileSync(`${__dirname}/views/index.html`, 'utf8')
+  .replace('[main.js]', `${cdn}app.js`)
+  .replace('[main.css]', `${cdn}app.css`);
 
 app.get('/', (req, res) => {
   res.send(indexView);
 });
 
-
-const compiler = webpack(config);
 
 app.use(webpackDevMiddleware(compiler, {
   publicPath: config.output.publicPath,
